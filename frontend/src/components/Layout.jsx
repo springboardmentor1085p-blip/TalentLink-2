839c1b08a84e701a39f4e536088db39309502c21
mdamill <losthts8@gmail.com>
2025-10-23 12:09:47 +0530

    Layout setup & UI added for navbar, register and login

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from './Navbar.jsx'; // We will create this component next
 
 function Layout() {
   return (
-    <div>
-      this the layout
+    <div className="min-h-screen flex flex-col">
+
+      <Navbar />
+
+      <main className="flex-grow">
+        <Outlet />
+      </main>
+
+      <footer className="bg-gray-100 p-4 text-center text-gray-500">
+        &copy; 2025 TalentLink. All rights reserved.
+      </footer>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
